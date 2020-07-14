178af16a7239548dfadfa5bd8b2894dc207a04ad
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-07-14 11:17:33 +0300

    Updates

---
+++ precache-manifest.c9c4d4baf3b0dbdd6b01cb8e3d1a78c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8b94b74469609abe768d4149371283",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09cd4e9dabca46dfe87b",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4af7d8882fcf58f27701",
+    "url": "./static/js/2.3b144016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3b144016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cd4e9dabca46dfe87b",
+    "url": "./static/js/main.9b7d2897.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
